ab59d786b1a52dfc741656352c79e075aa58d34d
Caio Pontalti <caiopontalti@hotmail.com>
2020-05-29 18:31:36 -0300

    finalizando formulario

--- Formularios/codigo-fonte-inicial/src/App.vue
+++ Formularios/codigo-fonte-inicial/src/App.vue
@@ -21,17 +21,29 @@
 
             <div class="form-group">
               <label>Nome:</label>
-              <input type="text" class="form-control" placeholder="Seu nome">
+              <input type="text" 
+                class="form-control" 
+                placeholder="Seu nome" 
+                v-model.trim="desenvolvedor.nome"
+              >
             </div>
 
             <div class="form-group">
               <label>Endereço de email:</label>
-              <input type="email" class="form-control" placeholder="Seu email">
+              <input type="email" 
+                class="form-control" 
+                placeholder="Seu email" 
+                v-model.trim="desenvolvedor.email"
+              >
             </div>
 
             <div class="form-group">
               <label>Idade:</label>
-              <input type="number" class="form-control" placeholder="Sua idade">
+              <input type="number" 
+                class="form-control" 
+                placeholder="Sua idade" 
+                v-model.number="desenvolvedor.idade"
+              >
             </div>
 
             <div class="form-group">
@@ -39,12 +51,20 @@
               <p>Gênero:</p>
 
               <div class="form-check form-check-inline">
-                <input type="radio" class="form-check-input" value="Masculino">
+                <input type="radio" 
+                  class="form-check-input" 
+                  value="Masculino"
+                  v-model="desenvolvedor.genero"
+                >
                 <label class="form-check-label">Masculino</label>
               </div>
 
               <div class="form-check form-check-inline">
-                <input type="radio" class="form-check-input" value="Feminino">
+                <input type="radio" 
+                  class="form-check-input" 
+                  value="Feminino"
+                  v-model="desenvolvedor.genero"
+                >
                 <label class="form-check-label">Feminino</label>
               </div>
 
@@ -52,8 +72,14 @@
 
             <div class="form-group">
               <label>Ocupação:</label>
-              <select class="form-control" placeholder="Seu email">
-                <option>Selecione uma opção...</option>
+              <select class="form-control" v-model="desenvolvedor.ocupacao">
+                <option value="" disabled>Selecione uma opção...</option>
+                <option v-for="(sel, i) in ocupacoes" 
+                  :key="i"
+                  :value="i"
+                >
+                  {{ i }} - {{ sel }}
+                </option>
               </select>
             </div>  
 
@@ -62,22 +88,37 @@
               <p>Tecnologias:</p>
 
               <div class="form-check form-check-inline">
-                <input type="checkbox" class="form-check-input" value="JavaScript">
+                <input type="checkbox" 
+                  class="form-check-input" 
+                  value="JavaScript" 
+                  v-model="desenvolvedor.linguagens">
                 <label class="form-check-label">JavaScript</label>
               </div>
 
-              <div class="form-check form-check-inline" value="Vue JS">
-                <input type="checkbox" class="form-check-input">
+              <div class="form-check form-check-inline" >
+                <input type="checkbox" 
+                  class="form-check-input" 
+                  value="Vue JS"
+                  v-model="desenvolvedor.linguagens"
+                >
                 <label class="form-check-label">Vue JS</label>
               </div>
 
               <div class="form-check form-check-inline">
-                <input type="checkbox" class="form-check-input" value="Vuex">
+                <input type="checkbox" 
+                  class="form-check-input" 
+                  value="Vuex"
+                  v-model="desenvolvedor.linguagens"
+                >
                 <label class="form-check-label">Vuex</label>
               </div>
 
               <div class="form-check form-check-inline">
-                <input type="checkbox" class="form-check-input" value="Vue Router">
+                <input type="checkbox" 
+                  class="form-check-input" 
+                  value="Vue Router"
+                  v-model="desenvolvedor.linguagens"
+                >
                 <label class="form-check-label">Vue Router</label>
               </div>
 
@@ -85,20 +126,41 @@
 
             <div class="form-group">
               <label>Resumo de perfil:</label>
-              <textarea class="form-control" placeholder="Conte-nos um pouco sobre você..."></textarea>
+              <textarea 
+                class="form-control" 
+                placeholder="Conte-nos um pouco sobre você..."
+                v-model="desenvolvedor.biografia"
+              ></textarea>
             </div>
+
+            <div>
+              <app-range label="Salário Pretendido"
+                v-model.number="desenvolvedor.salario"
+                min="0"
+                max="15000"
+                step="100"
+                :inputClass="{'form-control-range': true}">
+              </app-range>
+            </div>
+
+
 
             <div class="form-group">
 
               <div class="form-check form-check-inline">
-                <input type="checkbox" class="form-check-input">
+                <input type="checkbox" 
+                  class="form-check-input"
+                  v-model="desenvolvedor.notificacoes"
+                  true-value="Sim"
+                  false-value="Não"
+                >
                 <label class="form-check-label">Receber notificações por email</label>
               </div>
 
             </div>
 
-            <button class="btn btn-secondary">Resetar</button>
-            <button class="btn btn-success">Enviar</button>
+            <button class="btn btn-secondary" type="reset" @click="limpar">Resetar</button>
+            <button class="btn btn-success" @click.prevent="enviar">Enviar</button>
 
           </form>
 
@@ -114,20 +176,39 @@
             <div class="card-header">Dados</div>
 
             <ul class="list-group list-group-flush">
-              <li class="list-group-item"><strong>Nome:</strong></li>
-              <li class="list-group-item"><strong>Email:</strong> </li>
-              <li class="list-group-item"><strong>Idade:</strong></li>
-              <li class="list-group-item"><strong>Gênero:</strong> </li>
-              <li class="list-group-item"><strong>Ocupação:</strong> </li>
-              <li class="list-group-item"><strong>Tecnologias:</strong> </li>
-              <li class="list-group-item"><strong>Biografia:</strong> </li>
-              <li class="list-group-item"><strong>Receber notificações?</strong> </li>
+              <li class="list-group-item"><strong>Nome: </strong>{{ desenvolvedor.nome }}</li>
+              <li class="list-group-item"><strong>Email: </strong>{{ desenvolvedor.email }}</li>
+              <li class="list-group-item"><strong>Idade: </strong>{{ desenvolvedor.idade }}</li>
+              <li class="list-group-item"><strong>Gênero: </strong> {{ desenvolvedor.genero }}</li>
+              <li class="list-group-item"><strong>Ocupação: </strong> {{ desenvolvedor.ocupacao }}</li>
+              <li class="list-group-item">
+                <strong>Tecnologias: </strong> 
+                <ul>
+                  <li v-for="(lin, i) in desenvolvedor.linguagens" :key="i">
+                    {{ lin }}
+                  </li>
+                </ul>  
+              </li>
+              <li class="list-group-item">
+                <strong>Biografia: </strong> 
+                <pre>{{ desenvolvedor.biografia }}</pre>
+              </li>
+              <li class="list-group-item">
+                <strong>Receber notificações? </strong> 
+                {{ desenvolvedor.notificacoes }} 
+              </li>
+
+              <li class="list-group-item">
+                <strong>Salário</strong> 
+                {{ desenvolvedor.salario }} 
+              </li>
+
             </ul>
 
             <div class="card-header">Model</div>
 
             <div class="card-body">
-              <pre><code>{{ {'nome': 'Plínio Naves'} }}</code></pre>
+              <pre><code>{{ desenvolvedor }}</code></pre>
             </div>
 
           </div>
@@ -141,6 +222,48 @@
   </div>
 </template>
 
+<script>
+
+import AppRange from './components/Range.vue'
+
+export default {
+  components:{
+    AppRange
+  },
+  data(){
+    return{
+      desenvolvedor:{
+        nome: 'Caio',
+        email:'caiopontalti@gmail.com',
+        idade: 32,
+        biografia:'',
+        genero:'Masculino',
+        notificacoes: 'Não',
+        linguagens:[],
+        ocupacao:'',
+        salario:1500
+      },
+      ocupacoes:[
+          'Front End (Web)',
+          'Front End (Mobile)',
+          'Front End (Web e Mobile)',
+          'Back End',
+          'Full Stack'
+        ]
+    }
+  },
+  methods:{ /* eslint-disable */
+    enviar(event){
+      const formDesenvolvedor = Object.assign({}, this.desenvolvedor) ;
+      console.log(formDesenvolvedor)
+    },
+    limpar(){
+      this.desenvolvedor.salario = 0
+
+    }
+  }
+}
+</script>
 
 <style scoped>
   .btn {
---
+++ Formularios/codigo-fonte-inicial/src/components/Range.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <label>{{ CustomLabel }}</label>
+        <input type="range"
+            v-bind="$attrs"
+            :value="value"
+            :class="inputClass"
+            @input="atualizar"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+    data(){
+        return{
+            valorAtual : this.value || this.$attrs.min
+        }
+    },
+    computed:{
+        CustomLabel(){
+            return `${ this.label } (R$ ${ this.valorAtual }) `
+        }
+    },
+    methods:{
+        atualizar(event){
+            const valor = event.target.value
+            this.$emit('input', valor)
+            this.valorAtual = valor
+        }
+    },
+    props:{
+        label: String,
+        value:[Number, String],
+        inputClass :[String, Object, Array]
+    }
+}
+</script>
