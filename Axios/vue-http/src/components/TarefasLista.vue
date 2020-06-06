19624767d48243a4d9f83e7dcf19150fbf8fd278
Caio Pontalti <caiopontalti@hotmail.com>
2020-06-06 20:07:19 -0300

    continuacao axios

---
+++ Axios/vue-http/src/components/TarefasLista.vue
@@ -0,0 +1,95 @@
+<template>
+    <div>
+        
+        <div class="row">
+            <div class="col-sm-10">
+                <h1 class="font-weight-light">Lista de Tarefas</h1>
+            </div>
+            <div class="col-sm-2">
+                <button class="btn btn-primary float-right" @click="exibirForm = !exibirForm">
+                    <i class="fa fa-plus mr-2"></i>
+                    <span>Criar</span>
+                </button>
+            </div>
+        </div>
+        
+
+        <ul class="list-group" v-if="tarefas.length > 0">
+            <TarefasListaIten
+                v-for="tarefa in tarefas"
+                :key="tarefa.id"
+                :tarefa="tarefa" 
+                @editar="selecionarTarefaEditar"
+                @deletar="deletarTarefa"/>
+        </ul>
+
+        <p v-else>Nenhuma tarefa criada.</p>
+
+        <TarefaSalvar 
+            v-if="exibirForm"
+            :tarefa="tarefaSelecionada"
+            @criar="criarTarefa"
+            @editar="editarTarefa"
+        />
+
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+import config from './../config/config'
+import TarefaSalvar from './TarefaSalvar.vue'
+import TarefasListaIten from './TarefasListaIten.vue'
+
+export default {
+    /* eslint-disable */
+    components: {
+        TarefaSalvar,
+        TarefasListaIten
+    },
+    data() {
+        return {
+            tarefas: [],
+            exibirForm:false,
+            tarefaSelecionada: undefined
+        }
+    },
+    created(){
+        axios.get(`${config.apiURL}/tarefas`).then((response) =>{
+            this.tarefas = response.data;
+        })
+    },
+    methods:{
+        criarTarefa(tarefa){
+            axios.post(`${config.apiURL}/tarefas`, tarefa).then(( response) =>{
+                this.tarefas.push(response.data)
+                this.resetar()
+            })
+        },
+        editarTarefa(tarefa){
+            axios.put(`${config.apiURL}/tarefas/${tarefa.id}`, tarefa).then( response =>{
+                const indice = this.tarefas.findIndex(t => t.id === tarefa.id)
+                this.tarefas.splice(indice, 1, response.data)
+                this.resetar()
+            })
+        },
+        deletarTarefa(tarefa){
+            axios.delete(`${config.apiURL}/tarefas/${tarefa.id}`).then(response =>{
+                const indice = this.tarefas.findIndex(t => t.id === tarefa.id)
+                this.tarefas.splice(indice, 1)
+                this.resetar()
+            })
+        },
+        selecionarTarefaEditar(tarefa){
+            this.tarefaSelecionada = tarefa
+            this.exibirForm = true
+        },
+        resetar(){
+            this.tarefaSelecionada = undefined
+            this.exibirForm = false
+        }
+    }
+}
+</script>
