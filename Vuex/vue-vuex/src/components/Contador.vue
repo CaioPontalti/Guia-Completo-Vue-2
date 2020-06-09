befcc16e068483d92d789a99216fb396e9e2acb9
Caio Pontalti <caiopontalti@hotmail.com>
2020-06-09 11:41:08 -0300

    continuacao vuex

--- Vuex/vue-vuex/src/components/Contador.vue
+++ Vuex/vue-vuex/src/components/Contador.vue
@@ -14,7 +14,9 @@ import {mapState} from 'vuex'
 
 export default {
     /* eslint-disable */ 
-    computed:mapState(['contador']),
+    computed:{
+        ...mapState(['contador'])
+    },
     methods:{
         decrementar(){
             this.$store.state.contador --
